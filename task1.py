21b5abb6c1f1619380f123510cd4aa81d037ea18
SofiaPodobedova <sovip@mail.ru>
2017-10-20 00:28:10 +0300

    Чтение с главной страницы

--- task1.py
+++ task1.py
@@ -1,7 +1,14 @@
 import requests
 import pdfminer.high_level
+from lxml import html
 
-r = requests.get('http://www.germania.diplo.de/contentblob/4048654/Daten/7889181/1entschiedenevisumantraege.pdf')
+embassy_link = requests.get('http://www.germania.diplo.de/Vertretung/russland/ru/02-mosk/1-visa/3-merkblaetter/nationale-visa/0-nationale-visa.html#topic23')
+parsed_embassy_link = html.fromstring(embassy_link.text)
+all_links = parsed_embassy_link.xpath('//a/@href')
+for i in all_links:
+    if '1entschiedenevisumantraege.pdf' in i:
+        link_to_list = 'http://www.germania.diplo.de' + i
+r = requests.get(link_to_list)
 with open('res.pdf', 'wb') as res:
     res.write(r.content)
 with open('res.pdf', 'rb') as doc:
